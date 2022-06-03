51aab6c6eff9966685e2d4c97f26ffa33e9894a8
wesujs <93000934+wesujs@users.noreply.github.com>
2022-06-03 14:15:20 -0400

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>
+                Hi, I'm Wes, nice to meet my new visitors. Enjoy your stay and
+                check out my work!
+              </p>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default About;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import GitHub from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import Mongo from "../assets/mongo.png";
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="lg:text-center text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">These are the technologies I've worked with</p>
+        </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="Javascript icon" />
+            <p>JAVASCRIPT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="Node.js icon" />
+            <p>NODE</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
+            <p>TAILWIND</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
+            <p>REACT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="Github icon" />
+            <p>GIT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="Mongo icon" />
+            <p>MONGO DB</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
